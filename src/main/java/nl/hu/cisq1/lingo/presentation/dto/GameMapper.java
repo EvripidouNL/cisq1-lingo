f0eb89e026c82a6a781c5a880f82eb3198abdad6
Evripidou <evripidounl@gmail.com>
2021-03-16 04:14:52 +0100

    Removed unused import

--- src/main/java/nl/hu/cisq1/lingo/presentation/dto/GameMapper.java
+++ src/main/java/nl/hu/cisq1/lingo/presentation/dto/GameMapper.java
@@ -3,7 +3,6 @@ package nl.hu.cisq1.lingo.presentation.dto;
 import nl.hu.cisq1.lingo.domain.Game;
 import nl.hu.cisq1.lingo.domain.Hint;
 import org.mapstruct.Mapper;
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface GameMapper {
