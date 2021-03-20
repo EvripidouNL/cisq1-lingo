3942dcba3a95f325e0c116a3dd2d5dcf67ff9e84
Evripidou <evripidounl@gmail.com>
2021-03-20 23:08:25 +0100

    makeGuess in TrainerControllerIntegrationTest

--- src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
@@ -1,9 +1,12 @@
 package nl.hu.cisq1.lingo.presentation;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.hu.cisq1.lingo.CiTestConfiguration;
 
 import nl.hu.cisq1.lingo.domain.Hint;
 import nl.hu.cisq1.lingo.presentation.dto.GameDTO;
+import nl.hu.cisq1.lingo.presentation.dto.GuessDTO;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,13 +14,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -35,7 +42,12 @@ class TrainerControllerIntegrationTest {
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().json("{'score' : 0, 'roundNumber' : 1, 'feedbackList' :[], 'attemptsLeft' : 5}"))
+                .andExpect(jsonPath("$.gameId").exists())
+                .andExpect(jsonPath("$.score").value(0))
+                .andExpect(jsonPath("$.roundNumber").value(1))
+                .andExpect(jsonPath("$.attemptsLeft").value(5))
+                .andExpect(jsonPath("$.feedbackList").value(new ArrayList<>()))
+                .andExpect(jsonPath("$.hint").exists())
                 .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
     }
 
@@ -43,11 +55,33 @@ class TrainerControllerIntegrationTest {
     @DisplayName("create a new round for a lingo game by id")
     void createRound() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders
-                .post("/lingo/game/{gameById}/round", 1L);
+                .post("/lingo/game/{gameById}/round", 1);
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().json("{'gameId' : 1, 'feedbackList' :[], attemptsLeft : 5}"))
+                .andExpect(jsonPath("$.gameId").exists())
+                .andExpect(jsonPath("$.score").value(0))
+                .andExpect(jsonPath("$.roundNumber").exists())
+                .andExpect(jsonPath("$.attemptsLeft").value(5))
+                .andExpect(jsonPath("$.feedbackList").value(new ArrayList<>()))
+                .andExpect(jsonPath("$.hint").exists())
+                .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
+    }
+
+    @Test
+    @DisplayName("make a guess on existing lingo game")
+    void makeGuess() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/lingo/game/{gameById}", 1).contentType(MediaType.APPLICATION_JSON).content("{\"attempt\": \"woord\"}");
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.gameId").exists())
+                .andExpect(jsonPath("$.score").exists())
+                .andExpect(jsonPath("$.roundNumber").exists())
+                .andExpect(jsonPath("$.attemptsLeft").exists())
+                .andExpect(jsonPath("$.feedbackList").exists())
+                .andExpect(jsonPath("$.hint").exists())
                 .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
     }
 }
