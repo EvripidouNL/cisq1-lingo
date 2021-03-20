88bbae1ae50a38afca09ed00c32d96158ac968f2
Evripidou <evripidounl@gmail.com>
2021-03-20 17:37:29 +0100

    added createGame ControllerIntegrationTest

--- src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package nl.hu.cisq1.lingo.presentation;
 
 import nl.hu.cisq1.lingo.CiTestConfiguration;
+import nl.hu.cisq1.lingo.domain.Game;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.hasLength;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -27,7 +33,8 @@ class TrainerControllerIntegrationTest {
                 .post("/lingo/create");
 
         mockMvc.perform(request)
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'score' : 0, 'roundNumber' : 1, 'attemptsLeft' : 5}"))
+                .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
     }
 }
