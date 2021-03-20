45f3922ebaa14dcb45e250c3ed0256f00c8b6b0d
Evripidou <evripidounl@gmail.com>
2021-03-20 21:11:40 +0100

    added createGame ControllerIntegrationTest

--- src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/presentation/TrainerControllerIntegrationTest.java
@@ -1,7 +1,9 @@
 package nl.hu.cisq1.lingo.presentation;
 
 import nl.hu.cisq1.lingo.CiTestConfiguration;
-import nl.hu.cisq1.lingo.domain.Game;
+
+import nl.hu.cisq1.lingo.domain.Hint;
+import nl.hu.cisq1.lingo.presentation.dto.GameDTO;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.Matchers.hasLength;
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -34,7 +35,19 @@ class TrainerControllerIntegrationTest {
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().json("{'score' : 0, 'roundNumber' : 1, 'attemptsLeft' : 5}"))
+                .andExpect(content().json("{'score' : 0, 'roundNumber' : 1, 'feedbackList' :[], 'attemptsLeft' : 5}"))
+                .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
+    }
+
+    @Test
+    @DisplayName("create a new round for a lingo game")
+    void createRound() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/lingo/game/{gameById}/round", 1L);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'gameId' : 1, 'feedbackList' :[], attemptsLeft : 5}"))
                 .andExpect(jsonPath("$.*", Matchers.hasSize(6)));
     }
 }
