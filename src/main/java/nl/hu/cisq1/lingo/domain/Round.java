73972770aba9204e377688be58a9a3c3091748ed
Evripidou <evripidounl@gmail.com>
2021-04-05 02:01:12 +0200

    guessWord function updated

--- src/main/java/nl/hu/cisq1/lingo/domain/Round.java
+++ src/main/java/nl/hu/cisq1/lingo/domain/Round.java
@@ -60,20 +60,52 @@ public class Round {
             throw new WordAlreadyGuessedException();
         }
 
+        List<Integer> letterMatch = new ArrayList<>();
+        // letterMatch to check if a guessedLetter is correct or not correct
+        List<Character> lettersRemain = new ArrayList<>();
+        // lettersRemain to check if a guessedLetter is present or absent
         List<Mark> marks = new ArrayList<>();
+        // to show the right marks to the user
 
-        for (int i=0; i< this.word.getLength(); i++) {
-            if(word.getLength() != attempt.length()) {
-                marks.add(Mark.INVALID);
-                continue;
-            }
+        for (int index = 0; index < this.word.getLength(); index++) {
+            Character letterOfGuess = attempt.charAt(index);
+            Character letterOfWord = this.word.getValue().charAt(index);
 
-            if (attempt.charAt(i) == word.getValue().charAt(i)) {
-                marks.add(Mark.CORRECT);
-            } else if (word.getValue().contains(attempt.charAt(i) + "")) {
-              marks.add(Mark.PRESENT);
+            if (!letterOfGuess.equals(letterOfWord)) {
+                // if the letter of the guess doesn't match the letter of the word add 0 (false)
+                letterMatch.add(0);
             } else {
-                marks.add(Mark.ABSENT);
+                // if the letter of the guess doesn't match the letter of the word add 1 (true)
+                letterMatch.add(1);
+            }
+        }
+
+        for (int index = 0; index < attempt.length(); index++) {
+            Character letterOfWord = this.word.getValue().charAt(index);
+
+            if (letterMatch.get(index).equals(0)) {
+                // if the match is false add the letter to lettersRemain arrayList
+                lettersRemain.add(letterOfWord);
+            }
+        }
+
+        for (int index = 0; index < attempt.length(); index++) {
+            Character letterOfGuess = attempt.charAt(index);
+
+            if (letterMatch.get(index).equals(0)) {
+                // check for the letters that were not correct
+                if (lettersRemain.contains(letterOfGuess)) {
+                    // if the guess has letters that are also in the lettersRemain
+                    marks.add(Mark.PRESENT);
+                    // remove the present letters of lettersRemain
+                    lettersRemain.remove(letterOfGuess);
+                } else {
+                    // if the guess has no letters that are in lettersRemain
+                    marks.add(Mark.ABSENT);
+                }
+            } else if (letterMatch.get(index).equals(1)) {
+                // if the guessedLetter is equal to the letterMatch
+                marks.add(Mark.CORRECT);
             }
         }
 
