a4417e08788d3cb01335f51f55deb8389bd86f07
Evripidou <evripidounl@gmail.com>
2021-04-05 16:56:19 +0200

    Corrected guessWord

--- src/main/java/nl/hu/cisq1/lingo/domain/Round.java
+++ src/main/java/nl/hu/cisq1/lingo/domain/Round.java
@@ -64,10 +64,26 @@ public class Round {
             throw new WordAlreadyGuessedException();
         }
 
-        Feedback feedback = new Feedback(attempt, generateMarks(attempt));
-        feedbacks.add(feedback);
+        if (attempt.length() == word.getLength()) {
+            Feedback feedback = new Feedback(attempt, generateMarks(attempt));
+            feedbacks.add(feedback);
+            return feedback;
+        } else {
+            Feedback feedback = new Feedback(attempt, generateInvalidMarks(attempt));
+            feedbacks.add(feedback);
+            return feedback;
+        }
+    }
 
-        return feedback;
+    private List<Mark> generateInvalidMarks(String attempt) {
+        List<Mark> marks = new ArrayList<>();
+        // to show the invalid marks to the user
+
+        for (int i=0; i< attempt.length(); i++) {
+            marks.add(Mark.INVALID);
+        }
+
+        return marks;
     }
 
     private List<Mark> generateMarks(String attempt) {
@@ -76,16 +92,15 @@ public class Round {
         List<Mark> marks = new ArrayList<>();
         // to show the right marks to the user
 
-        if (attempt.length() == word.getLength()) {
-            for (int index = 0; index < attempt.length(); index++) {
-                Character letterOfGuess = attempt.charAt(index);
-                Character letterOfWord = this.word.getValue().charAt(index);
+        for (int index = 0; index < attempt.length(); index++) {
+            Character letterOfGuess = attempt.charAt(index);
+            Character letterOfWord = this.word.getValue().charAt(index);
 
-                if (!letterOfGuess.equals(letterOfWord)) {
-                    // if the match is false add the letter to lettersRemain arrayList
-                    lettersRemain.add(letterOfWord);
-                }
+            if (!letterOfGuess.equals(letterOfWord)) {
+                // if the match is false add the letter to lettersRemain arrayList
+                lettersRemain.add(letterOfWord);
             }
+        }
 
             for (int index = 0; index < attempt.length(); index++) {
                 Character letterOfGuess = attempt.charAt(index);
@@ -103,12 +118,6 @@ public class Round {
                     marks.add(Mark.CORRECT);
                 }
             }
-        } else {
-            // if the attempt length is not the same as the word length
-            for (int i=0; i< attempt.length(); i++) {
-                marks.add(Mark.INVALID);
-            }
-        }
 
         return marks;
     }
